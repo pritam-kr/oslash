d8c86c2f9a6cba073ba976d89732e014d81ebee8
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-07-07 20:43:02 +0530

    Initial Commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+   <nav className='flex justify-center items-center'>
+        
+   </nav>
+  )
+}
+
+export default React.memo(Nav)
---
+++ src/data/movies.js
@@ -0,0 +1,71 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const movies = {
+    allMovies: [
+        {
+            id: uuidv4(),
+            title: "Doctor Strange in the Multiverse of Madness",
+            image: "https://image.tmdb.org/t/p/w1280/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
+            ticketPrice: 350,
+        },
+
+        {
+            id: uuidv4(),
+            title: "Minions: The Rise of Gru",
+            image: "https://image.tmdb.org/t/p/w1280/wKiOkZTN9lUUUNZLmtnwubZYONg.jpg",
+            ticketPrice: 350,
+        },
+
+        {
+            id: uuidv4(),
+            title: "The Ledge",
+            image: "https://image.tmdb.org/t/p/w1280/dHKfsdNcEPw7YIWFPIhqiuWrSAb.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Fantastic Beasts: The Secrets of Dumbledore",
+            image: "https://image.tmdb.org/t/p/w1280/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Sonic the Hedgehog 2",
+            image: "https://image.tmdb.org/t/p/w1280/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Jurassic World Dominion",
+            image: "https://image.tmdb.org/t/p/w1280/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Dog",
+            image: "https://image.tmdb.org/t/p/w1280/rkpLvPDe0ZE62buUS32exdNr7zD.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "The Man From Toronto",
+            image: "https://image.tmdb.org/t/p/w1280/uTCfTibqtk4f90cC59bLPMOmsfc.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Morbius",
+            image: "https://image.tmdb.org/t/p/w1280/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg",
+            ticketPrice: 350,
+        },
+        {
+            id: uuidv4(),
+            title: "Spider-Man: No Way Home",
+            image: "https://image.tmdb.org/t/p/w1280/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+            ticketPrice: 350,
+        },
+
+    ]
+};
+
+
