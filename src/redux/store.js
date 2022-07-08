7d861602b24cdbb6cd40136b56ab985d63c10622
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-07-08 18:21:59 +0530

    Update store.js

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore } from "redux";
+import { shitReducer } from "./reducer/shitReducer";
+
+ 
+ const allReducer = combineReducers({shitReducer})
+
+ export const store = createStore(allReducer)
