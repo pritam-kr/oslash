12a72e22e414cd65142f475e4e580ea90f9ad92e
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-07-08 20:48:22 +0530

    Refactor

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const Nav = () => {
   return (
     <nav className="flex justify-center items-center border-b-[2px] p-4 bg-[#fff]">
-      <h3 className="text-2xl">oSlash Movies</h3>
+      <h3 className="text-2xl"><Link to="/">oSlash Movies</Link></h3>
     </nav>
   );
 };
