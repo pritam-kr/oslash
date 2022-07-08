3aa62b5baf21aff25399336d54dde0ecdba32703
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-07-08 18:21:28 +0530

    Data updated

---
+++ src/data/allShits.js
@@ -0,0 +1,246 @@
+import { v4 as uuidv4 } from 'uuid';
+export const allShits = {
+
+    shits: [  {
+        side: "X",
+        price: 100,
+        chairs: [
+          {
+            id: uuidv4(),
+            shitNumber: 1
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 2
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 3
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 4
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 5
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 6
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 7
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 8
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 9
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 10
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 11
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 12
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 13
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 14
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 15
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 16
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 17
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 18
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 19
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 20
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 21
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 22
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 23
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 24
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 25
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 26
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 27
+          },
+          {
+            id: uuidv4(),
+            shitNumber: 28
+          },
+          
+        ], 
+    }, 
+
+    {
+        side: "Y",
+        price: 80,
+        chairs: [
+            {
+              id: uuidv4(),
+              shitNumber: 1
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 2
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 3
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 4
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 5
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 6
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 7
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 8
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 9
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 10
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 11
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 12
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 13
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 14
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 15
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 16
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 17
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 18
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 19
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 20
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 21
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 22
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 23
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 24
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 25
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 26
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 27
+            },
+            {
+              id: uuidv4(),
+              shitNumber: 28
+            },
+           
+  
+          ], 
+    }
+]
+
+}
