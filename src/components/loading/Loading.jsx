53f3e9667fab307feaa5407ade36f7d785ea299f
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-07-08 23:28:21 +0530

    Feat: Loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Loading = () => {
   return (
     <div className="loading flex flex-col justify-center items-center my-4">
-      <img className="w-40 h-40 rounded-full opacity-0.3"
-        src={"https://assets.vogue.com/photos/5891938d58aa89a00d5446e4/master/pass/ultimate-popcorn-guide-for-oscar-movie-watching-square.gif"}
+      <img
+        className="w-40 h-40 rounded-full opacity-0.3"
+        src={
+          "https://res.cloudinary.com/dhqxln7zi/image/upload/v1650990392/Loading_icon_stoagp.gif"
+        }
         alt="gif"
       />
-      <h1 className="mt-4">Please! Book a Movie Ticket.</h1>
+      <h1 className="mt-4 mb-4">Please! Book a Movie Ticket.</h1>
+
+      <div className="btn-container text-center mt-8 mb-4">
+        <Link
+          to="/"
+          className="btn-booking bg-[#0662b3] px-6 py-2 rounded-sm text-[#fff] hover:bg-[#0662b6]"
+        >
+          Home
+        </Link>
+      </div>
     </div>
   );
 };
